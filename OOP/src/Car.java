803c962fdf589a798814012c553fcb3e7bcc371c
Juan Oliveira <ruantos@proton.me>
2023-09-15 23:20:14 -0300

    Introdução a OOP em Java

---
+++ OOP/src/Car.java
@@ -0,0 +1,12 @@
+public class Car {
+    String color = "yellow";
+    String model = "chevrolet";
+    short year = 1953;
+
+    void drive(){
+        System.out.println("The car is moving");
+    }
+    void stop(){
+        System.out.println("The car stopped");
+    }
+}
---
+++ OOP/src/Main.java
@@ -0,0 +1,10 @@
+public class Main{
+    public static void main(String[] args){
+        Car carro = new Car();
+        System.out.println(carro.color);
+        System.out.println(carro.model);
+        carro.drive();
+        carro.stop();
+
+    }
+}
